9ad2dca69f30798d241eb80613be61c8346aea66
julian <julian.batongbacal@gmail.com>
2021-04-13 19:56:56 +1000

    made agent objects

---
+++ src/util/AgentObjects.js
@@ -0,0 +1,128 @@
+import valorantLogo from '../images/valorantLogo.png';
+import jettLogo from '../images/jett.png';
+import phoenixLogo from '../images/phoenix.png';
+import razeLogo from '../images/raze.png';
+import reynaLogo from '../images/reyna.png';
+import yoruLogo from '../images/yoru.png';
+import astraLogo from '../images/astra.png';
+import brimstoneLogo from '../images/brimstone.png';
+import omenLogo from '../images/omen.png';
+import viperLogo from '../images/viper.png';
+import breachLogo from '../images/breach.png';
+import skyeLogo from '../images/skye.png';
+import sovaLogo from '../images/sova.png';
+import cypherLogo from '../images/cypher.png';
+import killjoyLogo from '../images/killjoy.png';
+import sageLogo from '../images/sage.png';
+
+const emptyAgent = {
+  img: valorantLogo,
+  name: 'Agent',
+  code: 'oo',
+  role: null,
+};
+
+const jett = {
+  img: jettLogo,
+  name: 'Jett',
+  code: 'je',
+  role: 'Duelist',
+};
+
+const phoenix = {
+  img: phoenixLogo,
+  name: 'Phoenix',
+  code: 'ph',
+  role: 'Duelist',
+};
+
+const raze = {
+  img: razeLogo,
+  name: 'Raze',
+  code: 'ra',
+  role: 'Duelist',
+};
+
+const reyna = {
+  img: reynaLogo,
+  name: 'Reyna',
+  code: 're',
+  role: 'Duelist',
+};
+
+const yoru = {
+  img: yoruLogo,
+  name: 'Yoru',
+  code: 'yo',
+  role: 'Duelist',
+};
+
+const astra = {
+  img: astraLogo,
+  name: 'Astra',
+  code: 'as',
+  role: 'Controller',
+};
+
+const brimstone = {
+  img: brimstoneLogo,
+  name: 'Brimstone',
+  code: 'bs',
+  role: 'Controller',
+};
+
+const omen = {
+  img: omenLogo,
+  name: 'Omen',
+  code: 'om',
+  role: 'Controller',
+};
+
+const viper = {
+  img: viperLogo,
+  name: 'Viper',
+  code: 'vi',
+  role: 'Controller',
+};
+
+const breach = {
+  img: breachLogo,
+  name: 'Breach',
+  code: 'br',
+  role: 'Initiator',
+};
+
+const skye = {
+  img: skyeLogo,
+  name: 'Skye',
+  code: 'sk',
+  role: 'Initiator',
+};
+
+const sova = {
+  img: sovaLogo,
+  name: 'Sova',
+  code: 'so',
+  role: 'Initiator',
+};
+
+const cypher = {
+  img: cypherLogo,
+  name: 'Cypher',
+  code: 'cy',
+  role: 'Sentinel',
+};
+
+const killjoy = {
+  img: killjoyLogo,
+  name: 'Killjoy',
+  code: 'kj',
+  role: 'Sentinel',
+};
+
+const sage = {
+  img: sageLogo,
+  name: 'Sage',
+  code: 'sa',
+  role: 'Sentinel',
+};
