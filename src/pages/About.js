c9783452e6d409c04f2f4e178054af394ece6282
batjuli <julian.batongbacal@gmail.com>
2022-10-12 22:11:32 +1100

    Prettier about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,20 +1,20 @@
-import { Typography } from '@material-ui/core';
-import React from 'react';
+import { Typography } from "@material-ui/core";
+import React from "react";
 
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 
 const About = () => {
   return (
     <Container
-      component='main'
-      maxWidth='md'
-      className='main'
-      style={{ color: '#FFFFFF' }}
+      component="main"
+      maxWidth="md"
+      className="main"
+      style={{ color: "#FFFFFF" }}
     >
-      <Typography variant='h5' noWrap style={{ color: '#FF4655' }}>
+      <Typography variant="h5" noWrap style={{ color: "#FF4655" }}>
         About
       </Typography>
-      <Typography variant='body1'>
+      <Typography variant="body1">
         Valcomps is a single page web app for planning and sharing your Valorant
         team comps.
         <br /> <br />
